3289f9c3abda469c491b452d9838bd3b1faf83a1
Dmitry Unkovsky <oil.crayons@gmail.com>
2017-03-02 20:12:40 +0200

    brute force improvements + tuning
      - increase input sequence length to 700
      - increase embeddings size to 300
      - increase max tokenizer dictionary size to 200000
      - increase batch size to 1024
      - add 0.4 dropout before final dense layer
      - change activation function to softmax
    
      - prev results
    
        train    val      test
        imdb     0.9016   0.7386
        rt       0.8211   0.8024
    
      - current results
    
        train    val      test
        imdb     0.8670   0.7427 (rt)
        rt       0.7987   0.7866 (imdb)

--- model-take-05/imdb.py
+++ model-take-05/imdb.py
@@ -1,4 +1,4 @@
-
+from __future__ import print_function
 
 import os
 import numpy as np
@@ -7,8 +7,9 @@ from keras.preprocessing.sequence import pad_sequences
 
 from keras.models import Model, Sequential, load_model
 from keras.layers import Input, Flatten
-from keras.layers import Dense, Activation, Embedding
+from keras.layers import Dense, Activation, Embedding, Dropout
 from keras.layers import LSTM, Bidirectional
+from keras.optimizers import Adam
 
 from keras.utils.np_utils import to_categorical
 from keras.callbacks import TensorBoard
@@ -18,13 +19,14 @@ from sklearn.model_selection import train_test_split
 import six.moves.cPickle
 
 BASE_DIR = ''
-GLOVE_DIR = BASE_DIR + '../../glove/' # http://nlp.stanford.edu/projects/glove/ pretrained vectors
-TEXT_DATA_DIR = '../../data/'
+GLOVE_DIR = BASE_DIR + '../glove/' # http://nlp.stanford.edu/projects/glove/ pretrained vectors
+TEXT_DATA_DIR = '../data/'
 HEADER = True
 
 
 import pandas as pd
 
+#df = pd.read_csv(TEXT_DATA_DIR + "imdb_small_head.csv", delimiter='|')
 df = pd.read_csv(TEXT_DATA_DIR + "imdb_small.csv", delimiter='|')
 X = df.text.as_matrix()
 y = df.label.astype(str).as_matrix()
@@ -67,16 +69,16 @@ y = df.label.astype(str).as_matrix()
 print("splitting data to train and test")
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-MAX_NB_WORDS = 20000
-EMBEDDING_DIM = 100
-MAX_SEQUENCE_LENGTH = 600
-BATCH_SIZE=32
-EPOCHS=10
+MAX_NB_WORDS = 200000
+EMBEDDING_DIM = 300
+MAX_SEQUENCE_LENGTH = 700
+BATCH_SIZE=1024
+EPOCHS=15
 
 
 tokenizer = Tokenizer(nb_words=MAX_NB_WORDS) # create dictionary of MAX_NB_WORDS, other words will not be used
-tokenizer.fit_on_texts(X)
-six.moves.cPickle.dump(tokenizer, open("tokenizer.pkl", "wb"))
+tokenizer.fit_on_texts(X_train)
+six.moves.cPickle.dump(tokenizer, open("tokenizer_imdb.pkl", "wb"))
 
 sequences = tokenizer.texts_to_sequences(X) # transform words to its indexes
 sequences_train = tokenizer.texts_to_sequences(X_train) # transform words to its indexes
@@ -98,7 +100,7 @@ print('Shape of label tensor:', labels.shape)
 
 
 embeddings_index = {}
-f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
+f = open(os.path.join(GLOVE_DIR, 'glove.6B.300d.txt'))
 for line in f:
     values = line.split()
     word = values[0]
@@ -106,14 +108,14 @@ for line in f:
     embeddings_index[word] = coefs
 f.close()
 
-if os.path.isfile("imdb_small.h5"):
+if os.path.isfile("imdb-model.h5"):
     print("Loading model...")
-    model = load_model("imdb_small.h5")
+    model = load_model("imdb-model.h5")
     print("done")
 else:
 
     # prepare embedding matrix
-    nb_words = min(MAX_NB_WORDS, len(word_index))
+    nb_words = min(MAX_NB_WORDS, len(word_index)+1)
     embedding_matrix = np.zeros((nb_words, EMBEDDING_DIM))
     for word, i in word_index.items():
         if i >= MAX_NB_WORDS:
@@ -135,25 +137,25 @@ else:
     #model.add(Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32'))
     model.add(embedding_layer)
     model.add(Bidirectional(LSTM(128, dropout_W=0.2, dropout_U=0.2)))
+    model.add(Dropout(0.5))
     model.add(Dense(2))
-    model.add(Activation('sigmoid'))
+    model.add(Activation('softmax'))
 
     model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
+                  optimizer=Adam(lr=0.0005),
                   metrics=['accuracy'])
 
     print('Train...')
     model.fit(data_train, labels_train, batch_size=BATCH_SIZE, nb_epoch=EPOCHS, validation_data=(data_test, labels_test),)
 
-
     score, acc = model.evaluate(data_test, labels_test,
                                 batch_size=BATCH_SIZE)
 
-    print('Test score:', score)
-    print('Test accuracy:', acc)
+    print("score, acc")
+    print("%.4f\t%.4f"%(score, acc))
+
 
     print("Saving model")
-    model.save("imdb_small.h5")
+    model.save("imdb-model.h5")
     print("done")
-
 
