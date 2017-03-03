f3f4f6b3475ddea1f4c0a6a8afe31d109c530de4
Dmitry Unkovsky <oil.crayons@gmail.com>
2017-03-03 10:27:19 +0200

    add checkpointing, early stopping and lr decrease
      - two layers
      - decrease input
      - one epoch is ~120s
      - stopper made it for 50 epochs
    
      train on imdb, test on rt
      val       test
      0.8917    0.7432

--- model-take-05/imdb.py
+++ model-take-05/imdb.py
@@ -12,7 +12,7 @@ from keras.layers import LSTM, Bidirectional
 from keras.optimizers import Adam
 
 from keras.utils.np_utils import to_categorical
-from keras.callbacks import TensorBoard
+from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping, ReduceLROnPlateau
 
 from sklearn.model_selection import train_test_split
 
@@ -31,76 +31,38 @@ df = pd.read_csv(TEXT_DATA_DIR + "imdb_small.csv", delimiter='|')
 X = df.text.as_matrix()
 y = df.label.astype(str).as_matrix()
 
-# TRAIN_LEN=75000
-# TEST_LEN=25000
-
-# X = []
-# y = []
-
-# X_train = []
-# y_train = []
-# 
-# X_test = []
-# y_test = []
-
-# i=0
-# with open(os.path.join(TEXT_DATA_DIR, "imdb_small.csv"), "r") as f:
-#     if HEADER:
-#         header = next(f)
-#     for line in f:
-#         temp_y, temp_x = line.rstrip("\n").split("|")
-# 
-#         X.append(temp_x)
-#         y.append(temp_y)
-# 
-#         i+=1
-
-# indices = np.arange(TRAIN_LEN)
-# np.random.shuffle(indices)
-# 
-# for i in indices:
-#     X_train.append(X[i])
-#     y_train.append(y[i])
-# 
-# for i in range(TRAIN_LEN, TRAIN_LEN+TEST_LEN): #len(X)):
-#     X_test.append(X[i])
-#     y_test.append(y[i])
-
 print("splitting data to train and test")
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 MAX_NB_WORDS = 200000
-EMBEDDING_DIM = 300
-MAX_SEQUENCE_LENGTH = 700
-BATCH_SIZE=1024
-EPOCHS=15
-
+EMBEDDING_DIM = 100
+MAX_SEQUENCE_LENGTH = 120
+BATCH_SIZE=2048
+EPOCHS=80
 
 tokenizer = Tokenizer(nb_words=MAX_NB_WORDS) # create dictionary of MAX_NB_WORDS, other words will not be used
 tokenizer.fit_on_texts(X_train)
 six.moves.cPickle.dump(tokenizer, open("tokenizer_imdb.pkl", "wb"))
 
-sequences = tokenizer.texts_to_sequences(X) # transform words to its indexes
 sequences_train = tokenizer.texts_to_sequences(X_train) # transform words to its indexes
 sequences_test = tokenizer.texts_to_sequences(X_test) # transform words to its indexes
 
 word_index = tokenizer.word_index # dictionary of word:index
 print('Found %s unique tokens.' % len(word_index))
 
-data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH) # transform a list to numpy array with shape (nb_samples, MAX_SEQUENCE_LENGTH)
-                                                            # be careful because it takes only last MAX_SEQUENCE_LENGTH words
+#data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH) # transform a list to numpy array with shape (nb_samples, MAX_SEQUENCE_LENGTH)
+#                                                            # be careful because it takes only last MAX_SEQUENCE_LENGTH words
 data_train = pad_sequences(sequences_train, maxlen=MAX_SEQUENCE_LENGTH)
 data_test = pad_sequences(sequences_test, maxlen=MAX_SEQUENCE_LENGTH)
                                                             
-labels = to_categorical(np.asarray(y))
 labels_train = to_categorical(np.asarray(y_train))
 labels_test = to_categorical(np.asarray(y_test))
-print('Shape of data tensor:', data.shape)
-print('Shape of label tensor:', labels.shape)
+print('Shape of data tensor:', data_train.shape)
+print('Shape of label tensor:', labels_train.shape)
 
 
 embeddings_index = {}
-f = open(os.path.join(GLOVE_DIR, 'glove.6B.300d.txt'))
+f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
 for line in f:
     values = line.split()
     word = values[0]
@@ -136,17 +98,25 @@ else:
     model = Sequential()
     #model.add(Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32'))
     model.add(embedding_layer)
-    model.add(Bidirectional(LSTM(128, dropout_W=0.2, dropout_U=0.2)))
-    model.add(Dropout(0.5))
+    model.add(Bidirectional(LSTM(96, dropout_W=0.2, dropout_U=0.2, return_sequences=True)))
+    model.add(Dropout(0.3))
+    model.add(Bidirectional(LSTM(32, dropout_W=0.2, dropout_U=0.2)))
+    model.add(Dropout(0.3))
     model.add(Dense(2))
     model.add(Activation('softmax'))
 
     model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(lr=0.0005),
+                  optimizer=Adam(lr=0.001),
                   metrics=['accuracy'])
 
+    stopper_cb = EarlyStopping(monitor='val_acc', min_delta=0, patience=8, verbose=1, mode='auto')
+    checkpoint_cb = ModelCheckpoint("checkpoint/weights.{epoch:02d}-{val_acc:.4f}-{val_loss:.4f}.h5", monitor='val_loss',
+                                    save_best_only=True, verbose=0)
+    slower_cb = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=4, verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
+
     print('Train...')
-    model.fit(data_train, labels_train, batch_size=BATCH_SIZE, nb_epoch=EPOCHS, validation_data=(data_test, labels_test),)
+    model.fit(data_train, labels_train, batch_size=BATCH_SIZE, nb_epoch=EPOCHS, validation_data=(data_test, labels_test),
+              callbacks=[stopper_cb, checkpoint_cb, slower_cb])
 
     score, acc = model.evaluate(data_test, labels_test,
                                 batch_size=BATCH_SIZE)
